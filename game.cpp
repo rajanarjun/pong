ff40ca9d44415c7e60840ec1ec0cfd33af839a58
Arjun Rajan <arjunrajan991@gmail.com>
2025-07-17 07:03:53 +0530

    formatting

--- game.cpp
+++ game.cpp
@@ -13,7 +13,8 @@ static SDL_Window* win = nullptr;
 static SDL_Renderer* renderer = nullptr;
 
 static Paddle player_1(0, (SCREEN_HEIGHT/2) - (PADDLE_HEIGHT / 2));
-static Paddle player_2((SCREEN_WIDTH - 30), (SCREEN_HEIGHT/2) - (PADDLE_HEIGHT / 2));
+static Paddle player_2((SCREEN_WIDTH - 30),
+                       (SCREEN_HEIGHT/2) - (PADDLE_HEIGHT / 2));
 
 int initialize_game() {
 	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
@@ -21,7 +22,11 @@ int initialize_game() {
 		return 1;
 	}
 
-	win = SDL_CreateWindow("Pong", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	win = SDL_CreateWindow("Pong", SDL_WINDOWPOS_CENTERED, 
+                                   SDL_WINDOWPOS_CENTERED, 
+                                   SCREEN_WIDTH, 
+                                   SCREEN_HEIGHT, 
+                                   SDL_WINDOW_SHOWN);
     if (win == nullptr) {
         cout << "Error creating window:" << SDL_GetError() << endl;
 		SDL_Quit();
